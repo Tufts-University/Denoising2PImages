8c6eab68af28b81cf6fafaefce42696474ff6845
Filippos Sakellariou <filip.sakel@icloud.com>
2022-08-08 12:05:48 -0400

    Update mem request on run script.

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp072
 #SBATCH --exclude=cc1gpu004
 #SBATCH --mem=20g  #requesting 2GB of RAM total
-#SBATCH --output=Denoising_monorepo_test.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=Denoising_monorepo_test.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../Denoising_monorepo_test.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../Denoising_monorepo_test.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=20193005@student.anatolia.edu.gr
 
@@ -21,4 +21,4 @@ cd Cervical-Project
 git pull
 echo "Starting python script..."
 echo "==========================================================\n"
-python -u main.py train 
+python -u main.py train
