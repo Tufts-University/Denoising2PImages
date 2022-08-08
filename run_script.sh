b29fc74377595bda83204b49717ade0006deab2c
Filippos Sakellariou <filip.sakel@icloud.com>
2022-08-08 11:36:04 -0400

    Add run script.

---
+++ run_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J FS_080822_Denoising   #job name
+#SBATCH --time=05-00:0:00  #requested time
+#SBATCH -p preempt     #running on "preempt" partition/queue
+#SBATCH -N 1    #1 nodes
+#SBATCH	-n 10   #10 tasks total
+#SBATCH	-c 1   #using 1 cpu core/task
+#SBATCH --gres=gpu:a100:1
+##SBATCH --nodelist=p1cmp072
+#SBATCH --exclude=cc1gpu004
+#SBATCH --mem=2g  #requesting 2GB of RAM total
+#SBATCH --output=pytestjob.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=pytestjob.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --mail-type=ALL    #email options
+#SBATCH --mail-user=20193005@student.anatolia.edu.gr
+
+module load anaconda/2021.05
+source activate Denoising
+
+python -u main.py train > Denoising_monorepo_test.out
