e739c08ac6e0d6f5c8edaf897fa1a1eec78bb329
Filippos Sakellariou <filip.sakel@icloud.com>
2022-08-09 16:37:15 -0400

    Change run script

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp072
 #SBATCH --exclude=cc1gpu004,cc1gpu002
 #SBATCH --mem=20g  #requesting 2GB of RAM total
-#SBATCH --output=../FAD_model_0713_cervix_SSIML1.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../FAD_model_0713_cervix_SSIML1.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../NADH_CAREmodel_0713_cervix_SSIMR2_Wavelet.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../NADH_CAREmodel_0713_cervix_SSIMR2_Wavelet.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=20193005@student.anatolia.edu.gr
 
@@ -24,7 +24,7 @@ echo "" # empty line
 
 # When changing an important parameter, change the name both here and in the output/error files (above SBATCH arguments).
 
-# NADH_CAREmodel_0713_cervix_SSIMR2_Wavelet:
-# python -u main.py train care "NADH_CAREmodel_0713_cervix_SSIMR2_Wavelet" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet=True
+python -u main.py train care "NADH_CAREmodel_0713_cervix_SSIMR2_Wavelet" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet=True
 
-python -u main.py eval care "FAD_model_0713_cervix_SSIML1" cwd=.. nadh_data=NV_713_FAD_healthy.npz
+# FAD_model_0713_cervix_SSIML1
+# python -u main.py eval care "FAD_model_0713_cervix_SSIML1" cwd=.. nadh_data=NV_713_FAD_healthy.npz
