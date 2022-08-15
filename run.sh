d13b9fec8ede96a6196dd222ef1485e693e3495e
Filippos Sakellariou <filip.sakel@icloud.com>
2022-08-15 11:16:32 -0400

    Train NADH_model_0713_cervix_SSIMPCC

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp072
 #SBATCH --exclude=cc1gpu004,cc1gpu002
 #SBATCH --mem=20g  #requesting 2GB of RAM total
-#SBATCH --output=../NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_haar.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_haar.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../NADH_model_0713_cervix_SSIMPCC.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../NADH_model_0713_cervix_SSIMPCC.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=20193005@student.anatolia.edu.gr
 
@@ -48,7 +48,7 @@ echo "" # empty line #
 ### MARK: Different Wavelet Functions ############################
 
 # NADH RCAN SSIMR2 ap5 Wavelet Haar ✅ ⏰
-python -u main.py eval rcan "NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_haar" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet_function=haar
+# python -u main.py eval rcan "NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_haar" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet_function=haar
 
 # NADH RCAN SSIMR2 ap5 Wavelet Morl ❌ continuous wavelet issue (no cwt2 func)
 # python -u main.py train rcan "NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_morl" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet_function=morl
@@ -67,7 +67,7 @@ python -u main.py eval rcan "NADH_model_0713_cervix_SSIMR2_ap5_Wavelet_haar" cwd
 ### PCC ##########################################################
 
 # NADH RCAN SSIMPCC ⏰
-# python -u main.py eval rcan "NADH_model_0713_cervix_SSIMPCC" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimpcc_loss
+python -u main.py train rcan "NADH_model_0713_cervix_SSIMPCC" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimpcc_loss
 
 ##################################################################
 
