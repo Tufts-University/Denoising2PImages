df046eb384bdea417e93b6e0143cc52475b803e8
Filippos Sakellariou <filip.sakel@icloud.com>
2022-08-16 13:02:37 -0400

    Eval NADH_model_0713_cervix_SSIMPCC(_dup)

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 #SBATCH -N 1    #1 nodes
 #SBATCH	-n 10   #10 tasks total
 #SBATCH	-c 1   #using 1 cpu core/task
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:a100:1
 ##SBATCH --nodelist=p1cmp072
 #SBATCH --exclude=cc1gpu004,cc1gpu002
 #SBATCH --mem=20g  #requesting 2GB of RAM total
-#SBATCH --output=../NADH_SRGAN.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../NADH_SRGAN.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../NADH_model_0713_cervix_SSIMPCC.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../NADH_model_0713_cervix_SSIMPCC.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=20193005@student.anatolia.edu.gr
 
@@ -65,9 +65,9 @@ echo "" # empty line #
 
 ### PCC ##########################################################
 
-# NADH RCAN SSIMPCC (ap84) ✅ (Though Premptied) — 
-# python -u main.py eval rcan "NADH_model_0713_cervix_SSIMPCC" cwd=.. nadh_data=NV_713_NADH_healthy.npz fad_data=NV_713_FAD_healthy.npz loss=ssimpcc_loss loss_alpha=0.84
-# python -u main.py eval rcan "NADH_model_0713_cervix_SSIMPCC_dup" cwd=.. nadh_data=NV_713_NADH_healthy.npz fad_data=NV_713_FAD_healthy.npz loss=ssimpcc_loss loss_alpha=0.84
+# NADH RCAN SSIMPCC (ap84) ✅ (Though Premptied) ⏰
+python -u main.py eval rcan "NADH_model_0713_cervix_SSIMPCC" cwd=.. nadh_data=NV_713_NADH_healthy.npz fad_data=NV_713_FAD_healthy.npz loss=ssimpcc_loss loss_alpha=0.84
+python -u main.py eval rcan "NADH_model_0713_cervix_SSIMPCC_dup" cwd=.. nadh_data=NV_713_NADH_healthy.npz fad_data=NV_713_FAD_healthy.npz loss=ssimpcc_loss loss_alpha=0.84
 
 # NADH RCAN SSIMPCC ap5 ⏰ 
 # python -u main.py train rcan "NADH_model_0713_cervix_SSIMPCC_ap5" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimpcc_loss loss_alpha=0.5
@@ -77,7 +77,7 @@ echo "" # empty line #
 ### SRGAN ########################################################
 
 # SRGAN Trial 1
-python -u srgan.py
+# python -u srgan.py
 
 ##################################################################
 
