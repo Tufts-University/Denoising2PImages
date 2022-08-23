5894f00f32cce8c05ed099cf4a5289b269397174
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-08-23 15:59:49 -0400

    Temp patch for hardcoded val_seed

--- data_generator.py
+++ data_generator.py
@@ -501,10 +501,8 @@ def load_training_data(file, validation_split=0, axes=None, n_images=None,
     ################################################################################
     ######################Testing on older models!##################################
     new_test = range(0,int(SE[6])+1)
-    ranges = np.hstack((new_test,range(int(SE[-5]),len(X))))
-    print(len(ranges))
+    ranges = np.hstack((new_test,range(int(SB[-5]),len(X))))
     X_t, Y_t = X[ranges],  Y[ranges]
-    print(len(X_t))
     validation_idx = np.hstack((range(0,7),range(-5,0)))
     num_stacks = [int(SE[x]-SB[x]+1)/4 for x in validation_idx]
     prev = 0
