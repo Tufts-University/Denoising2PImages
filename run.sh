3f311d50e2af5270574b5c91a93d28d4439dbb91
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-08-25 11:53:17 -0400

    Evaluating NADH_CAREmodel_0713_cervix_SSIMR2_ap5_Wavelet_bior1p1_deep using new data

--- run.sh
+++ run.sh
@@ -83,7 +83,7 @@ echo "" # empty line #
 # python -u main.py train care "NADH_CAREmodel_0713_cervix_SSIMR2_ap5_Wavelet_bior1p1_deep_fs3" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet_function=bior1.1 loss_alpha=0.5
 # python -u main.py eval care "NADH_CAREmodel_0713_cervix_SSIMR2_ap5_Wavelet_bior1p1_deep_fs3" cwd=.. fad_data=NV_713_FAD_healthy.npz nadh_data=NV_713_NADH_healthy.npz wavelet_function=bior1.1
 
-# NADH CARE + SSIMR2 ap5 Wavelet bior1.1 SSIM deep (new_data) ✅ ⏰
+# NADH CARE + SSIMR2 ap5 Wavelet bior1.1 SSIM deep (new_data) ✅ ✅
 # python -u main.py train care "NADH_CAREmodel_0713_cervix_SSIMR2_ap5_Wavelet_bior1p1_deep_fs3" cwd=.. nadh_data=NV_713_NADH_healthy.npz loss=ssimr2_loss wavelet_function=bior1.1 loss_alpha=0.5
 python -u main.py eval care "NADH_CAREmodel_0713_cervix_SSIMR2_ap5_Wavelet_bior1p1_deep" cwd=.. fad_data=NV_823_FAD_healthy.npz nadh_data=NV_823_NADH_healthy.npz wavelet_function=bior1.1 val_seed=0
 
