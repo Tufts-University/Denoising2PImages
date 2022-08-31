fb00ba475f78390748b103cfeec9f684f78d988c
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-08-31 17:21:22 -0400

    Changing the use of VGG to CARE since VGG is for RGB images and we only have grayscale

--- train.py
+++ train.py
@@ -54,7 +54,6 @@ def fit_model(model, model_name, config, output_dir, training_data, validation_d
         checkpoint_filepath = 'weights_{epoch:03d}_{val_loss:.8f}.hdf5'
     else:
         checkpoint_filepath = 'weights_{epoch:03d}_{loss:.8f}.hdf5'
-
     model.fit(
         x=training_data if model_name != 'care' else training_data[0],
         y=None if model_name != 'care' else training_data[1],
