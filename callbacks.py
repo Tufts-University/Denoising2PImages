418e2b45a5b86e0e833c91a1b973fc1888af185f
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-09-01 12:21:57 -0400

    Training and Eval of NADH_Resnet_0823_cervix_SSIMR2_ap5
    Former-commit-id: ac7ac4a3b920d5c8664de21e4673d8edfcd23553

--- callbacks.py
+++ callbacks.py
@@ -86,6 +86,9 @@ def get_callbacks(model_name, epochs, output_dir, checkpoint_filepath, validatio
     elif model_name == 'srgan':
         learning_rate_callback = tf.keras.callbacks.ReduceLROnPlateau(
             verbose=True, factor=0.97, min_delta=0, patience=20)
+    elif model_name == 'resnet':
+        learning_rate_callback = tf.keras.callbacks.ReduceLROnPlateau(
+            verbose=True, factor=0.97, min_delta=0, patience=20)
     else:
         raise ValueError(f'Unknown model name: {model_name}')
 
