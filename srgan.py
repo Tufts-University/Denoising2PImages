57b9749ae782244674ce9b693baefd5b541102b6
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-09-05 13:09:18 -0400

    Fixing path name
    Former-commit-id: 3a0ae86f0ddf7671731c7c38583d72ed104cfb18

--- srgan.py
+++ srgan.py
@@ -113,7 +113,7 @@ def SRGAN_fit_model(model_name, strategy, config, initial_path,output_dir,traini
         CARE_final_weights_path = str(initial_path /pathlib.Path(output_dir) / 'CARE_Pretrained.hdf5')
     else: 
         if os.path.exists((initial_path + '/Denoising2PImages/' + 'CARE_Pretrained.hdf5')):
-            CARE_final_weights_path = str(initial_path / 'Denoising2PImages' / 'CARE_Pretrained.hdf5')
+            CARE_final_weights_path = (initial_path + '/Denoising2PImages/' + 'CARE_Pretrained.hdf5')
             print(f'CARE Pretrained weights found in GitLab Repository path :{CARE_final_weights_path}')
         else:
             raise Exception('CARE Model needs to be pretrained, please confirm you have weights for standard CARE model')
