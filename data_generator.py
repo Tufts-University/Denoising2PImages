a85dd5b355f72a2a23ca79a8cf876ca71f7d74ca
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-09-08 12:21:36 -0400

    Attempting to figure out why we are getting the wrong training set
    Former-commit-id: 378c9476ba09a5cfc9da8b879aec0c358105c7ca

--- data_generator.py
+++ data_generator.py
@@ -527,7 +527,7 @@ def load_training_data(file, validation_split=4, split_seed=0, testing_split=8, 
             prev += te_num_stacks[i]
 
         # Generation of Training Set
-        temp_idx = sorted(np.concatenate([validation_idx,train_idx],axis=0))
+        temp_idx = sorted(np.concatenate([validation_idx,test_idx],axis=0))
         print(f'Removing Test and Validation Set from loaded data: {temp_idx}')
         temp_idx = np.flip(temp_idx)
         for i in range(len(temp_idx)):
