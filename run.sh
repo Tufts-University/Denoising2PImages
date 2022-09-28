d9da4d8eacd8cd70b672f33e94bb75180b700b07
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-09-28 16:52:36 -0400

    Train and Eval of NADH_CARE_0928_cervix_SSIML2_seed1
    Former-commit-id: e4198e832679c7ad9bfe270b7aa3b79b84af974d

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp110
 #SBATCH --exclude=cc1gpu005
 #SBATCH --mem=20g  #requesting 2GB of RAM total 
-#SBATCH --output=../NADH_CARE_0928_cervix_SSIML2_seed0.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../NADH_CARE_0928_cervix_SSIML2_seed0.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../NADH_CARE_0928_cervix_SSIML2_seed1.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../NADH_CARE_0928_cervix_SSIML2_seed1.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=nvora01@tufts.edu
 
@@ -26,7 +26,11 @@ echo "" # empty line #
 ### MARK: FAD Model — NADH Eval ##################################
 
 ## RUN with Config:
-# NADH CARE SSIML2 deep seed 0 ✅ ✅ ✅ ✅        
+# NADH CARE SSIML2 deep seed 0 ⏰ ⏰ 40685896   
+# NADH CARE SSIML2 deep seed 1 ⏰ ⏰ 
+# NADH CARE SSIML2 deep seed 2 ⏰ ⏰
+# NADH CARE SSIML2 deep seed 3 ⏰ ⏰ 
+# NADH CARE SSIML2 deep seed 4 ⏰ ⏰         
 python -u main.py config.json
 python -u main.py config.json mode=eval fad_data=NV_928_FAD_Testing.npz nadh_data=NV_928_NADH_Testing.npz
 
