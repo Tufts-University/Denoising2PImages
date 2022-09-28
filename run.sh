27db9d60adcb016eed2ea0c995052306b46c4ff0
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-09-28 16:53:12 -0400

    Train and Eval of NADH_CARE_0928_cervix_SSIML2_seed2
    Former-commit-id: cb751831111e4d1d7b0eee45f1ed1c7638263e56

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp110
 #SBATCH --exclude=cc1gpu005
 #SBATCH --mem=20g  #requesting 2GB of RAM total 
-#SBATCH --output=../NADH_CARE_0928_cervix_SSIML2_seed1.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../NADH_CARE_0928_cervix_SSIML2_seed1.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../NADH_CARE_0928_cervix_SSIML2_seed2.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../NADH_CARE_0928_cervix_SSIML2_seed2.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=nvora01@tufts.edu
 
@@ -27,7 +27,7 @@ echo "" # empty line #
 
 ## RUN with Config:
 # NADH CARE SSIML2 deep seed 0 ⏰ ⏰ 40685896   
-# NADH CARE SSIML2 deep seed 1 ⏰ ⏰ 
+# NADH CARE SSIML2 deep seed 1 ⏰ ⏰ 40685900
 # NADH CARE SSIML2 deep seed 2 ⏰ ⏰
 # NADH CARE SSIML2 deep seed 3 ⏰ ⏰ 
 # NADH CARE SSIML2 deep seed 4 ⏰ ⏰         
