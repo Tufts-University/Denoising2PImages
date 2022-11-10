2c1b185b0894cd1db375e5c865aadd535450119b
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2022-11-10 15:25:38 -0500

    Murine Data Eval using NADH_CARE_0823_cervix_MAEWavelet_seed0
    Former-commit-id: 51ff1c57a70d17ef8aa077b554254835cad36c2c

--- run.sh
+++ run.sh
@@ -65,8 +65,11 @@ echo "" # empty line #S
 # NADH CARE MAE deep seed 0 ⏰ ⏰ 40984831      
 # python -u main.py eval care "NADH_CARE_0823_cervix_MAE_seed0" cwd=.. fad_data=NV_LFN_FAD_Testing.npz nadh_data=NV_LFN_NADH_Testing.npz loss="mae" val_seed=0 val_split=25 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 
 
+# # NADH CARE MAEWavelet deep seed 0 ⏰ ⏰ 40984831      
+# python -u main.py eval care "NADH_CARE_0823_cervix_MAEWavelet_seed0" cwd=.. fad_data=NV_LFN_FAD_Testing.npz nadh_data=NV_LFN_NADH_Testing.npz loss="mae" wavelet_function="bior1.1" val_seed=0 val_split=25 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 
+
 # NADH CARE MAEWavelet deep seed 0 ⏰ ⏰ 40984831      
-python -u main.py eval care "NADH_CARE_0823_cervix_MAEWavelet_seed0" cwd=.. fad_data=NV_LFN_FAD_Testing.npz nadh_data=NV_LFN_NADH_Testing.npz loss="mae" wavelet_function="bior1.1" val_seed=0 val_split=25 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 
+python -u main.py eval care "NADH_CARE_0823_cervix_MAEWavelet_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz nadh_data=NV_Murine_NADH_Testing.npz loss="mae" wavelet_function="bior1.1" val_seed=0 val_split=25 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 
 
 # FAD RCAN SSIM ✅ ✅
 # python -u main.py eval rcan "FAD_model_0629_cervix_SSIM" cwd=.. nadh_data=NV_713_NADH_healthy.npz
