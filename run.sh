d660ddb592875a17dbf23ab937f3a4d8afab5220
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2023-01-05 10:53:32 -0500

    Eval of human and mouse data using CARE SSIM
    Former-commit-id: 720c93b4b6b1da06b3f70ec997dd0da377ac8358

--- run.sh
+++ run.sh
@@ -41,12 +41,12 @@ python -u main.py eval care "FAD_CARETesting_0928_cervix_SSIM_new_seed0" cwd=.. 
 # NADH CARE Testing SSIM Murine
 python -u main.py train care "NADH_CARE_0823_Murine_seed0" cwd=.. nadh_data=NV_Murine_NADH_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=0 ssim_FSize=11 ssim_FSig=1.5 loss="ssim_loss" train_mode=1
 
-python -u main.py eval wunet "NADH_CARE_0823_Murine_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz nadh_data=NV_Murine_NADH_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=1 ssim_FSize=11 ssim_FSig=1.5  loss="ssim_loss" train_mode=1
+python -u main.py eval wunet "NADH_CARE_0823_Murine_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz nadh_data=NV_Murine_NADH_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=0 ssim_FSize=11 ssim_FSig=1.5 loss="ssim_loss" train_mode=1
 
 # FAD CARE Testing SSIM Murine
 python -u main.py train care "FAD_CARE_0823_Murine_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=0 ssim_FSize=11 ssim_FSig=1.5 loss="ssim_loss" train_mode=1
 
-python -u main.py eval wunet "FAD_CARE_0823_Murine_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz nadh_data=NV_Murine_NADH_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=1 ssim_FSize=11 ssim_FSig=1.5  loss="ssim_loss" train_mode=1
+python -u main.py eval wunet "FAD_CARE_0823_Murine_seed0" cwd=.. fad_data=NV_Murine_FAD_Testing.npz nadh_data=NV_Murine_NADH_Testing.npz val_seed=0 val_split=2 test_split=4 test_flag=0 ssim_FSize=11 ssim_FSig=1.5 loss="ssim_loss" train_mode=1
 
 # FAD CARE Testing Wavelet SSIMR2
 # python -u main.py eval wunet "FAD_CARETesting_Wavelet_0928_cervix_SSIMR2_new_seed0" cwd=.. fad_data=NV_1213_FAD_Testing.npz nadh_data=NV_1213_NADH_Testing.npz loss="ssimr2_loss" val_seed=0 val_split=8 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 test_flag=1 train_mode=0 wavelet_function=bior1.1
