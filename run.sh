263cc3645f6094885f3c1e8355fe3ac18e5e58b4
Nilay Vora <38589990+runseefly@users.noreply.github.com>
2023-02-14 17:17:32 -0500

    Train and Eval of FAD_UnetRCAN_0928_cervix_MAE
    Former-commit-id: 42a0a1e82b4c71b9d00f4c7c50fd373d69a7e757

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@
 ##SBATCH --nodelist=p1cmp110
 #SBATCH --exclude=cc1gpu005
 #SBATCH --mem=30g  #requesting 2GB of RAM total 
-#SBATCH --output=../FAD_CARETesting_Wavelet_0210_cervix_SSIMR2_new_seed0.%j.out  #saving standard output to file -- %j jobID -- %N nodename
-#SBATCH --error=../FAD_CARETesting_Wavelet_0210_cervix_SSIMR2_new_seed0.%j.err  #saving standard error to file -- %j jobID -- %N nodename
+#SBATCH --output=../FAD_UnetRCAN_0928_cervix_MAE.%j.out  #saving standard output to file -- %j jobID -- %N nodename
+#SBATCH --error=../FAD_UnetRCAN_0928_cervix_MAE.%j.err  #saving standard error to file -- %j jobID -- %N nodename
 #SBATCH --mail-type=ALL    #email options
 #SBATCH --mail-user=nvora01@tufts.edu
 
@@ -26,7 +26,11 @@ echo "" # empty line #
 
 # NADH UnetRCAN Testing MSE 
 # python -u main.py train UnetRCAN "NADH_UnetRCAN_0928_cervix_MSE" cwd=.. nadh_data=NV_928_NADH_Training.npz  loss="mse"  val_seed=0 val_split=8 test_flag=1 train_mode=0 num_residual_blocks=5 num_residual_groups=4
-python -u main.py eval UnetRCAN "NADH_UnetRCAN_0928_cervix_MSE" cwd=.. fad_data=NV_928_FAD_Testing.npz nadh_data=NV_928_NADH_Testing.npz loss="mse" val_seed=0 val_split=8 test_flag=1 train_mode=0 num_residual_blocks=5 num_residual_groups=4
+# python -u main.py eval UnetRCAN "NADH_UnetRCAN_0928_cervix_MSE" cwd=.. fad_data=NV_928_FAD_Testing.npz nadh_data=NV_928_NADH_Testing.npz loss="mse" val_seed=0 val_split=8 test_flag=1 train_mode=0 num_residual_blocks=5 num_residual_groups=4
+
+# FAD UnetRCAN Testing MAE 
+python -u main.py train UnetRCAN "FAD_UnetRCAN_0928_cervix_MAE" cwd=.. fad_data=NV_928_FAD_Training.npz  loss="mae"  val_seed=0 val_split=8 test_flag=1 train_mode=0 num_residual_blocks=5 num_residual_groups=4
+python -u main.py eval UnetRCAN "FAD_UnetRCAN_0928_cervix_MAE" cwd=.. fad_data=NV_928_FAD_Testing.npz nadh_data=NV_928_NADH_Testing.npz loss="mae" val_seed=0 val_split=8 test_flag=1 train_mode=0 num_residual_blocks=5 num_residual_groups=4
 
 # FAD CARE SSIMR2 Testing MSE 
 # python -u main.py eval wunet "FAD_CARETesting_Wavelet_0928_cervix_SSIMR2_new_seed0" cwd=.. fad_data=NV_0210_FAD_Testing.npz nadh_data=NV_0210_NADH_Testing.npz loss="ssimr2_loss" val_seed=0 val_split=8 ssim_FSize=3 ssim_FSig=0.5 loss_alpha=0.84 test_flag=1 train_mode=0 wavelet_function=bior1.1
